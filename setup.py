12e9f15dbf73dc7b3b9d9074d46f0bb40399f0ed
jda74 <jda7407@gmail.com>
2024-01-02 00:10:56 +0900

    add long_description

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='sqlalchemy-nest',
@@ -7,6 +14,8 @@ setup(
     url='https://github.com/satorudev976/sqlalchemy-nest.git',
     packages=find_packages(),
     install_requires=["sqlalchemy>=0.7"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='satoru',
     license='MIT',
     python_requires='>=3.9'
