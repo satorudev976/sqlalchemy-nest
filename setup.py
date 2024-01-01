3e4f1b06a5b5834c925e5e8bb0cb67b8cf8de4a3
jda74 <jda7407@gmail.com>
2024-01-01 22:32:17 +0900

    add nest model constructor

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sqlalchemy-nest',
+    version='0.1.0',
+    description='easy create nested models for sqlalchemy',
+    url='https://github.com/satorudev976/sqlalchemy-nest.git',
+    packages=find_packages(),
+    author='satoru',
+    license='MIT',
+    python_requires='>=3.9'
+)
---
+++ sqlalchemy_nest/__init__.py
@@ -0,0 +1,43 @@
+from typing import Any
+from sqlalchemy.orm import class_mapper
+
+def declarative_nested_model_constructor(self: Any, **kwargs: Any) -> None:
+    """A nested model support constructor that allows initialization from kwargs.
+
+    Sets attributes on the constructed instance using the names and
+    values in ``kwargs``.
+
+    attributes of the instance's class are allowed. These could be,
+    for example, any mapped columns or relationships or composites.
+    """
+    cls_ = type(self)
+    
+    mapped_columns = class_mapper(cls_).columns
+    relationships = class_mapper(cls_).relationships
+    composites = class_mapper(cls_).composites
+    
+    for k in kwargs:
+        if not hasattr(cls_, k):
+            continue
+        if k in mapped_columns:
+            setattr(self, k, kwargs[k])
+            continue
+        elif k in relationships: 
+            relation_cls = relationships[k].mapper.entity
+            if isinstance(kwargs[k], list): # "one-to-many"
+                for elem in kwargs[k]:
+                    if isinstance(elem, dict):
+                        instances = [relation_cls(**elem) for elem in kwargs[k]]
+                        setattr(self, k, instances)
+                    else:
+                        setattr(self, k, kwargs[k])
+            elif isinstance(kwargs[k], dict): # "one-to-one"
+                instance = relation_cls(**kwargs[k])
+                setattr(self, k, instance)
+        elif k in composites:
+            composite_cls = composites[k].composite_class
+            instance = composite_cls(**kwargs[k])
+            setattr(self, k, instance)
+        else:
+            setattr(self, k, kwargs[k])
+    
