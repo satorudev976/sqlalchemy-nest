9cca3a3db51bbdcfdafaeaa2bc94b601feaa62b1
jda74 <jda7407@gmail.com>
2024-01-03 16:38:39 +0900

    relase version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='sqlalchemy-nest',
-    version='0.1.0',
+    version='1.0.0',
     description='easy create nested models for sqlalchemy',
     url='https://github.com/satorudev976/sqlalchemy-nest.git',
     packages=find_packages(),
