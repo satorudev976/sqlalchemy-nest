2c687761a36df7fa699fd6831e9bc329447276f3
jda74 <jda7407@gmail.com>
2024-01-12 00:44:32 +0900

    テストコードの簡略化

--- tests/test_sqlalchemy_nest.py
+++ tests/test_sqlalchemy_nest.py
@@ -1,6 +1,6 @@
 import pytest
 from datetime import date
-from tests.models import Base, Branch, DateRange, Leaf, Node, RegistrationCard, Reservation, Root
+from tests.models import Branch, DateRange, Leaf, Node, RegistrationCard, Reservation, Root
     
 
 class TestOneToMany:
@@ -85,6 +85,17 @@ class TestOneToMany:
 
 class TestOneToOne:
     
+    expect_reservation = Reservation(
+        id=1,
+        start_date=date(2024, 1, 1),
+        end_date=date(2024, 1, 2),
+        registration_card=RegistrationCard(
+            id=1,
+            guest_name='Jon',
+            reservation_id=1
+            )
+        )
+    
     @pytest.fixture(autouse=True, scope="function")
     def setup(self, session):
         yield
@@ -98,7 +109,6 @@ class TestOneToOne:
     @pytest.mark.parametrize(
         "reservation",
         [
-            # 
             pytest.param({
                 'start_date': date(2024, 1, 1),
                 'end_date': date(2024, 1, 2),
@@ -132,19 +142,8 @@ class TestOneToOne:
             session.add(Reservation(**reservation))
             session.commit()
             new_reservation: Reservation = session.query(Reservation).filter(Reservation.id == 1).first()
-            
-            expect_reservation = Reservation(
-                id=1,
-                start_date=date(2024, 1, 1),
-                end_date=date(2024, 1, 2),
-                registration_card=RegistrationCard(
-                    id=1,
-                    guest_name='Jon',
-                    reservation_id=1
-                )
-            )
-        
-            assert new_reservation == expect_reservation
+                    
+            assert new_reservation == TestOneToOne.expect_reservation
     
     
     @pytest.mark.parametrize(
@@ -176,10 +175,4 @@ class TestOneToOne:
             session.commit()
             new_reservation: Reservation = session.query(Reservation).filter(Reservation.id == 1).first()
             
-            assert new_reservation.id == 1
-            assert new_reservation.start_date == date(2024, 1, 1)
-            assert new_reservation.end_date == date(2024, 1, 2)
-            assert new_reservation.registration_card.id == 1
-            assert new_reservation.registration_card.reservation_id == new_reservation.id
-            assert new_reservation.registration_card.guest_name == 'Jon'
-    
+            assert new_reservation == TestOneToOne.expect_reservation
