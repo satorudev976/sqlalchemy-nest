7c850c2da4ab43c4c57535b61c5aefb14945196c
jda74 <jda7407@gmail.com>
2024-01-13 21:33:29 +0900

    [refactor] mypy error

--- sqlalchemy_nest/orm.py
+++ sqlalchemy_nest/orm.py
@@ -2,6 +2,7 @@ from typing import Any
 from sqlalchemy.orm import class_mapper
 from sqlalchemy.orm.properties import RelationshipProperty
 
+
 class BaseModel(object):
     
     def merge(self, **kwargs: Any) -> None:
@@ -18,38 +19,36 @@ class BaseModel(object):
                 continue
             
             if isinstance(kwargs.get(relationship.key), list) or isinstance(getattr(self, relationship.key), list):
-                self._merge_one_to_many_relationship(relationship, **kwargs)
+                if kwargs.get(relationship.key):
+                    self._merge_one_to_many_relationship(relationship, kwargs.get(relationship.key))
+                else:
+                    setattr(self, relationship.key, [])
             else:
-                self._merge_one_to_one_relationship(relationship, **kwargs)
+                if kwargs.get(relationship.key):
+                    self._merge_one_to_one_relationship(relationship, kwargs.get(relationship.key))
+                else:
+                    setattr(self, relationship.key, None)
 
-    def _merge_one_to_one_relationship(self, relationship: RelationshipProperty[Any], **kwargs: Any):
-        if kwargs.get(relationship.key):
-            relationship_cls = getattr(self, relationship.key)
-            if relationship_cls:
-                relationship_cls.merge(**kwargs.get(relationship.key))
-            else:
-                setattr(self, relationship.key, relationship.mapper.entity(**kwargs.get(relationship.key)))
+    def _merge_one_to_one_relationship(self, relationship: RelationshipProperty[Any], value):
+        relationship_cls = getattr(self, relationship.key)
+        if relationship_cls:
+            relationship_cls.merge(**value)
         else:
-            setattr(self, relationship.key, None)
+            setattr(self, relationship.key, relationship.mapper.entity(**value))
 
-    def _merge_one_to_many_relationship(self, relationship: RelationshipProperty[Any], **kwargs: Any):
-        if kwargs.get(relationship.key):
-            relationship_cls = getattr(self, relationship.key)
-            pks = relationship.entity.primary_key
-            should_remove_entities = relationship_cls[:]
-            for elem in kwargs.get(relationship.key):
-                if all(elem.get(pk.name) is None for pk in pks):
-                    relationship_cls.append(relationship.mapper.entity(**elem))
-                    continue
-                
-                for entity in relationship_cls:
-                    if all(getattr(entity, pk.name) == elem.get(pk.name) for pk in pks):
-                        entity.merge(**elem)
-                        should_remove_entities.remove(entity)
-
-            for should_remove_entity in should_remove_entities:
-                relationship_cls.remove(should_remove_entity)
-        else:
-            setattr(self, relationship.key, [])
-    
+    def _merge_one_to_many_relationship(self, relationship: RelationshipProperty[Any], values):
+        relationship_cls = getattr(self, relationship.key)
+        pks = relationship.entity.primary_key
+        should_remove_entities = relationship_cls[:]
+        for elem in values:
+            if all(elem.get(pk.name) is None for pk in pks):
+                relationship_cls.append(relationship.mapper.entity(**elem))
+                continue
             
+            for entity in relationship_cls:
+                if all(getattr(entity, pk.name) == elem.get(pk.name) for pk in pks):
+                    entity.merge(**elem)
+                    should_remove_entities.remove(entity)
+
+        for should_remove_entity in should_remove_entities:
+            relationship_cls.remove(should_remove_entity)
